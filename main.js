5812b717a527161756d28a4d7fc7a8b8e81d91d9
MikkMaltis <maltismikk@gmail.com>
2023-09-22 06:59:14 +0300

    commit

--- main.js
+++ main.js
@@ -4,10 +4,22 @@ const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
 const getData = () => {
-    axios.get('https://weatherapi-com.p.rapidapi.com/current.json').then(response => {
+    axios.get('https://reqres.in/api/users').then(response => {
         console.log(response);
     })
 };
+
+const sendData = () => {
+  axios.post('https://reqres.in/api/register', {
+    
+  })
+};
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(err, err.response);
+    });
 
 getBtn.addEventListener('click', getData);
 postBtn.addEventListener('click', sendData);
