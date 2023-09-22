bc5e4181610cab464281583b7388b30c23c0e794
MikkMaltis <maltismikk@gmail.com>
2023-09-22 07:21:26 +0300

    commit

--- main.js
+++ main.js
@@ -4,22 +4,27 @@ const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
 const getData = () => {
-    axios.get('https://reqres.in/api/users').then(response => {
-        console.log(response);
-    })
+    axios.get('https://reqres.in/api/users')
+        .then(response => {
+            console.log(response);
+        })
+        .catch(err => {
+            console.error(err);
+        });
 };
 
 const sendData = () => {
-  axios.post('https://reqres.in/api/register', {
-    
-  })
-};
+    axios.post('https://reqres.in/api/register', {
+        email: 'eve.holt@reqres.in',
+        password: 'pistol'
+    })
     .then(response => {
-      console.log(response);
+        console.log(response);
     })
     .catch(err => {
-      console.log(err, err.response);
+        console.error(err);
     });
+};
 
 getBtn.addEventListener('click', getData);
 postBtn.addEventListener('click', sendData);
